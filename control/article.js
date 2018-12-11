fc6d5ee15c476512d92cc15847a63ff982b50ad7
252020338 <252020338@qq.com>
2018-12-11 21:59:32 +0800

    文章分页

--- control/article.js
+++ control/article.js
@@ -9,28 +9,22 @@ const User = db.model('users', UserSchema)
 
 //返回文章发表页面
 exports.addArticlePage = async(ctx)=>{
-    console.log(1)
     await ctx.render("publishArticle", {
         session: ctx.session
     })
 }
 //添加发表文章
 exports.addArticle = async(ctx)=>{
-    console.log("添加发表文章")
     const data = ctx.request.body
-    console.log(data)
     data.author = ctx.session.uid
     await new Promise((resolve, reject) =>{
         new Article(data).save((err, data)=>{
-            // console.log(data.author)
             if(err){ return reject(err)}
-            console.log(data)
             resolve(data)
         })
     })
     .then( 
         async (data)=>{
-        console.log("发表成功")
         await ctx.render('back', {
             status: "发表成功",
             back: "即将跳转到首页",
@@ -39,7 +33,6 @@ exports.addArticle = async(ctx)=>{
     )})
     .catch( 
         async (err)=>{
-        console.log("发表失败")
         await ctx.render('back', {
             status: "发表失败",
             back: "文章标题已存在，请重新发表",
@@ -49,24 +42,26 @@ exports.addArticle = async(ctx)=>{
 }
 //返回文章列表
 exports.getList = async(ctx) =>{
-    console.log("000")
     let page = ctx.params.id || 1 //默认在第一页
         page--
+
+    const maxNum = await Article.estimatedDocumentCount((err,data)=>err? console.log(err):data) //获取数据库所有的数据
+    console.log(maxNum)
     let artList = await Article
         .find() //找到所有数据
         .sort("-created") //倒序排序
-        .skip(2 * page) //跳过 20*页数 的数据
-        .limit(2) //每页显示2条
+        .skip(3 * page) //跳过 20*页数 的数据
+        .limit(3) //每页显示2条
         .populate({
             path: "author",
             select: "_id username headPhoto"
         })
         .then(data => data)
         .catch(err =>console.log(err))
-    console.log(artList)
     await ctx.render("nav", {
         // title:title,
         session: ctx.session,
         artList: artList,
+        maxNum,
     })
 }
